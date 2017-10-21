e16398b908c1c168dc9ec9f8d9c5b5718cb4dce7
Jeff Hong Xiao <jeffhongxiao@gmail.com>
2017-10-21 13:39:02 -0300

    add test data and my solution for sequencial sublist problem

---
+++ 20171020_181542_002/jeffhongxiao.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+
+def parse_list(text):
+    splitted = text.split(',')
+    listA = [int(s) for s in splitted[0].split(' ')]
+    listB = [int(s) for s in splitted[1].split(' ')]
+    return listA, listB
+
+def decide_if_sublist(A, B):
+    lenB = len(B)
+    lenA = len(A)
+
+    index = []
+    for i in range(lenB):
+        if A[0] == B[i]:
+            index.append(i)
+
+    flag = True
+    for i in index:
+        for k in range(0, lenA):
+            if i+k >= lenB: 
+                return False
+            if A[k] != B[i+k]:
+                flag = False
+    return flag
+
+for line in sys.stdin:
+    text = line[:-1]	# remove EOF
+    listA, listB = parse_list(text)
+    print listA
+    print listB
+
+    if decide_if_sublist(listA, listB): 
+        print 'yes'
+    else:
+        print 'no'
+
+
